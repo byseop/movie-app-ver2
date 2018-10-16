0aff389696bd23cca7bf8f4a2f149b9626b0e5a9
byseop <byseop@gmail.com>
2018-10-16 16:31:51 +0900

    2-1. API 정리

--- src/component/MovieWrapper.js
+++ src/component/MovieWrapper.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import _ from 'lodash';
 
 @inject('store')
 @observer
 class MovieWrapper extends Component {
-
   componentDidMount(){
-    this.props.store._getMovies();
+    this.props.store._callApi();
   }
-
+  
   render() {
+    const store = this.props.store;
     return (
       <>
+      <div>{ _.isEmpty(store.movieList) ? 'a' : 'b' }</div>
       </>
     );
   }
--- src/store/MovieStore.js
+++ src/store/MovieStore.js
@@ -2,18 +2,15 @@ import { observable, action, configure } from 'mobx';
 import axios from 'axios';
 
 // action 을 통해서 상태변경
-configure({ enforceActions: true });
+configure({ enforceActions: 'observed' });
 
 class MovieStore {
-  @observable movieList = [];
+  @observable movieList = {};
   @action _callApi = () => {
     axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=dc11dbd0605b4d60cc66ce5e8363e063&language=ko-KR')
-      .then(response => console.log(response))
+      .then(action(response => this.movieList = response.data.results))
+      .then(json => console.log(json))
       .catch(err => console.log(err))
-  }
-  @action _getMovies = async() => {
-    const movies = await this._callApi();
-    this.movieList = movies;
   }
 }
 
