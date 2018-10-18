71c60474f5a391a8737a2182f5152272aa5b53e8
byseop <byseop@gmail.com>
2018-10-18 10:22:24 +0900

    2-3 API 통신에러 해결됨

--- src/store/MovieStore.js
+++ src/store/MovieStore.js
@@ -10,32 +10,32 @@ class MovieStore {
   @observable isMovieLoded = false;
   @observable sortMethod = '';
 
-  @action _callApi = () => {
+  @action _callApi = (sortPram) => {
+    let SORT = '';
     const NOW_PLAYING = '/now_playing';
     const DEFAULT_URL = 'https://api.themoviedb.org/3/movie';
     const API_KEY = '?api_key=dc11dbd0605b4d60cc66ce5e8363e063';
     const LANGUAGE_KR = '&language=ko-KR';
+
+    // eslint-disable-next-line to the line before.
+    if ( sortPram == '0') SORT = NOW_PLAYING // 소트0 -> 현재상영중
     
-    if (this.sortMethod === '0') { 
-      // 소트 0 -> 현재 상영중
-      return axios.get(DEFAULT_URL + NOW_PLAYING + API_KEY + LANGUAGE_KR)
-        .then (response => response.data)
-        .then (json => console.log(json))
-        .catch(err => console.log(err))
-    }
+    return axios.get(DEFAULT_URL + SORT + API_KEY + LANGUAGE_KR)
+      .then (response => response.data)
+      .catch(err => console.log(err))
+  }
+
+  @action _checkMovieLoad = (movieObj) => {
+    if ( !_.isEmpty(movieObj) ) this.isMovieLoded = !this.isMovieLoded;
+    else console.log('Error!');
   }
   
   @action _getMovies = async(sortPram) => {
-    this.sortMethod = sortPram;
-    const movies = await this._callApi();
-    // this.movieList = movies.results;
-    // console.log(this.movieList);
-    // this.checkMovieLoad(this.movieList);
-  }
-  @action _checkMovieLoad = (movieObj) => {
-    if ( !_.isEmpty(movieObj) ) this.isMovieLoded =! this.isMovieLoded;
-    else console.log('Error!');
-  }
+    const movies = await this._callApi(sortPram);
+    this.movieList = movies.results;
+    console.log(this.movieList);
+    this._checkMovieLoad(this.movieList);
+  }  
 }
 
 const store = new MovieStore();
