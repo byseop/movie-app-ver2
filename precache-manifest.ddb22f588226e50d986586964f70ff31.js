6922100a90c503e14c68c511a2b216b758418904
byseop <byseop@gmail.com>
2018-10-19 14:10:41 +0900

    Updates

---
+++ precache-manifest.ddb22f588226e50d986586964f70ff31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c3457c9110e4fe9f060b7b83f3ad59",
+    "url": "/movie-app-ver2/static/media/bg.22c3457c.png"
+  },
+  {
+    "revision": "42ddd53b597a21789462",
+    "url": "/movie-app-ver2/static/js/runtime~main.42ddd53b.js"
+  },
+  {
+    "revision": "fab30d2c967d194ed0b1",
+    "url": "/movie-app-ver2/static/js/main.fab30d2c.chunk.js"
+  },
+  {
+    "revision": "4a7d38da59c6a5874e28",
+    "url": "/movie-app-ver2/static/js/1.4a7d38da.chunk.js"
+  },
+  {
+    "revision": "fab30d2c967d194ed0b1",
+    "url": "/movie-app-ver2/static/css/main.c2067686.chunk.css"
+  },
+  {
+    "revision": "2f9d6c6dafa3d6cece41bfd562e69e88",
+    "url": "/movie-app-ver2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/movie-app-ver2/precache-manifest.4caded27c9b9428f0cb9fd649be25545.js"
+  "/movie-app-ver2/precache-manifest.ddb22f588226e50d986586964f70ff31.js"
 );
 
 workbox.clientsClaim();
